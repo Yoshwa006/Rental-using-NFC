42ff84c917243a6ebd49b99aaf4219554eeb2d45
yoshwa <ryoshwaa@gmail.com>
2025-02-11 00:37:42 +0530

    Added QR Service,, has more to update in this qr service

--- src/main/java/com/example/mongo/backend/controller/Controller.java
+++ src/main/java/com/example/mongo/backend/controller/Controller.java
@@ -3,12 +3,13 @@ package com.example.mongo.backend.controller;
 
 import com.example.mongo.backend.Item.Item;
 import com.example.mongo.backend.service.ItemService;
+import com.example.mongo.backend.service.QrService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -16,26 +17,45 @@ import java.util.Optional;
 public class Controller {
 
     private final ItemService service;
+    private final QrService qr;
 
     @Autowired
-    public Controller(ItemService service){
+    public Controller(ItemService service, QrService qr){
         this.service = service;
+        this.qr = qr;
     }
 
 
-    @GetMapping
-    public List<Item> returnAll(){
-        return service.returnAll();
-    }
     @GetMapping("/{id}")
-    public Optional<Item> getData(@PathVariable String id){
-        return service.getDetails(id);
+    public ResponseEntity<?> getData(@PathVariable String id){
+        Optional<Item> item = service.getDetails(id);
+        if(item.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Check again ID");
+        }
+        return ResponseEntity.ok(item);
     }
+
 
     @PostMapping
-    public String registerItem(@RequestBody Item item){
+    public ResponseEntity<?> registerItem(@RequestBody Item item) {
         service.registerItem(item);
-        return "https://nfc-rental-system2-1.onrender.com/api/items/" + item.getId();
+
+        String fileName = item.getId() + ".png";
+        String directory = "src/main/resources/static/";
+        String filePath = directory + fileName;
+
+        try {
+            QrService.generateQRCode("https://nfc-rental-system2-1.onrender.com/api/items/" + item.getId(), filePath);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error generating QR Code");
+        }
+
+        String fileUrl = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/static/")
+                .path(fileName)
+                .toUriString();
+
+        return ResponseEntity.ok(fileUrl);
     }
 
     @PutMapping("/{itemId}/modify")
