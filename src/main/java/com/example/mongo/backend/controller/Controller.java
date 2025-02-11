22fd14d78bf0032d484f9f057539f9ea32db7944
yoshwa <ryoshwaa@gmail.com>
2025-02-11 12:49:43 +0530

    Removed qr

--- src/main/java/com/example/mongo/backend/controller/Controller.java
+++ src/main/java/com/example/mongo/backend/controller/Controller.java
@@ -3,7 +3,6 @@ package com.example.mongo.backend.controller;
 
 import com.example.mongo.backend.Item.Item;
 import com.example.mongo.backend.service.ItemService;
-import com.example.mongo.backend.service.QrService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +16,12 @@ import java.util.Optional;
 public class Controller {
 
     private final ItemService service;
-    private final QrService qr;
+
 
     @Autowired
-    public Controller(ItemService service, QrService qr){
+    public Controller(ItemService service){
         this.service = service;
-        this.qr = qr;
+
     }
 
 
@@ -39,23 +38,7 @@ public class Controller {
     @PostMapping
     public ResponseEntity<?> registerItem(@RequestBody Item item) {
         service.registerItem(item);
-
-        String fileName = item.getId() + ".png";
-        String directory = "src/main/resources/static/";
-        String filePath = directory + fileName;
-
-        try {
-            QrService.generateQRCode("https://nfc-rental-system2-1.onrender.com/api/items/" + item.getId(), filePath);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error generating QR Code");
-        }
-
-        String fileUrl = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/static/")
-                .path(fileName)
-                .toUriString();
-
-        return ResponseEntity.ok(fileUrl);
+        return ResponseEntity.ok().build();
     }
 
     @PutMapping("/{itemId}/modify")
